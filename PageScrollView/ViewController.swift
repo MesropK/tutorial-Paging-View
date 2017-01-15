2c9c28cca34915cdb065027863f342478696a873
Mesrop Kareyan <Mesrop@Mesbook.local>
2017-01-15 11:43:31 +0400

    Initial Commit

--- PageScrollView/ViewController.swift
+++ PageScrollView/ViewController.swift
@@ -10,16 +10,49 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var scrollView: UIScrollView!
+
+    struct Constants {
+        static let iconHeight: CGFloat = 150.0
+    }
+    var images:[UIImageView] = []
+    
+    override func loadView() {
+        super.loadView()
+        initImages()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        scrollView.clipsToBounds = false
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        setupImages()
     }
-
-
+    
+    func initImages() {
+        for index in 0...2 {
+            let image = UIImage(named: "icon\(index)")
+            let imageView = UIImageView(image: image)
+            scrollView.addSubview(imageView)
+            images.append(imageView)
+        }
+    }
+    
+    func setupImages() {
+        var scrollViewContentWidth: CGFloat = 0
+        let scrollViewWidth = scrollView.frame.width
+        for (index, imageView) in images.enumerated() {
+            let extraDis: CGFloat  = UIDevice.current.orientation.isLandscape ? 0 : 0
+            let newX = (scrollView.center.x - Constants.iconHeight) / 2 +
+                        CGFloat(index) *
+                        (scrollViewWidth + extraDis)
+            imageView.frame = CGRect(x: newX, y: scrollView.center.y - Constants.iconHeight / 2, width: Constants.iconHeight, height: Constants.iconHeight)
+            scrollViewContentWidth += scrollViewWidth
+        }
+        scrollView.contentSize = CGSize(width: scrollViewContentWidth, height: scrollView.frame.width)
+    }
 }
 
